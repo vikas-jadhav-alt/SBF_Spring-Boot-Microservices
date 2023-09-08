3ed39fcece00440deb3b52ad9edf5fb0f7b895dd
Vikas Jadhav <vikas-jadhav@king.com>
2023-09-08 23:03:00 +0530

    Natraj Sir and Amiscode added

---
+++ SBF_SpringSecurity-BasicAuth/src/main/java/com/demo/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.demo.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class SecurityConfig extends WebSecurityConfiguration {
+
+}
