7136c4e6e6b7dbf826a20261a45882de8ce01df0
Vikas Jadhav <vikas-jadhav@king.com>
2023-10-09 11:43:34 +0530

    Spring Data JPA: Code Buffer Project Downloaded and Put in the GitHub

---
+++ SBF_SpringData_JPA/src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/SbfSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.spring.data.jpa.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbfSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbfSpringDataJpaApplication.class, args);
+	}
+
+}
